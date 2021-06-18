9185fb2ff3526211e3a98b389c9473e733a03078
hetalpatel29 <80544632+hetalpatel29@users.noreply.github.com>
2021-06-18 15:40:12 +0530

    my practice

---
+++ js/queryselector.js
@@ -0,0 +1,4 @@
+let n = document.querySelector('#heading').classList.add('olive');
+let a = document.querySelector('#subheading').classList.add('blue');
+let b = document.querySelector('#name').classList.toggle('menubar');
+let c = document.querySelector('#button').classList.toggle('bagcolor')[4];
